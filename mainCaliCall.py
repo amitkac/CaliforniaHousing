110c67d8f910878973623560b0720d3ca7751940
Amit Kachroo <amit.kachroo1987@gmail.com>
2020-02-22 11:47:04 -0600

    main call to run the program

---
+++ mainCaliCall.py
@@ -0,0 +1,30 @@
+from dataRead.loadData import LoadData
+import pandas as pd
+from dataProcessors import mercFxn
+from Plotting import plots
+
+
+URL = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz"
+
+
+def main():
+
+    dat = LoadData(URL)
+    LoadData.load_housing_data(dat)
+    df = dat.housingData
+
+    #  data cleaning
+
+    print(df.head())
+
+    mercCordObj = mercFxn.MercFx(df)
+    x, y = mercCordObj.mercatorCord()
+    z = plots.Plots(df, x, y)
+    z.holoViewPlot()
+
+
+if __name__ == '__main__':
+    main()
+
+
+# print(x[1:10])
